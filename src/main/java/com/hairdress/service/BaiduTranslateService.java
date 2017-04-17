a1394f86b8deb89cd18ed80dd997837ec77bce54
coldxiangyu <coldxiangyu@qq.com>
2017-04-17 20:37:25 +0800

    modified BaiduTranslateService.java add TransApi.java

--- src/main/java/com/hairdress/service/BaiduTranslateService.java
+++ src/main/java/com/hairdress/service/BaiduTranslateService.java
@@ -1,19 +1,39 @@
 package com.hairdress.service;
 
 import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import com.google.gson.Gson;
 import com.hairdress.model.TranslateResult;
+import com.hairdress.util.TransApi;
 
 /**
  * 
  */
 public class BaiduTranslateService {
+	
+	protected static final int SOCKET_TIMEOUT = 10000; // 10S
+    protected static final String GET = "GET";
+    private static final String APP_ID = "20170417000045048";
+    private static final String SECURITY_KEY = "6qteFVMiXLl8unvHPfU4";
 	/**
 	 * 发起http请求获取返回结果
 	 * 
@@ -21,55 +41,136 @@ public class BaiduTranslateService {
 	 *            请求地址
 	 * @return
 	 */
-	public static String httpRequest(String requestUrl) {
-		StringBuffer buffer = new StringBuffer();
-		try {
-			URL url = new URL(requestUrl);
-			HttpURLConnection httpUrlConn = (HttpURLConnection) url.openConnection();
+	public static String get(String host, Map<String, String> params) {
+        try {
+            // 设置SSLContext
+            SSLContext sslcontext = SSLContext.getInstance("TLS");
+            sslcontext.init(null, new TrustManager[] { myX509TrustManager }, null);
 
-			httpUrlConn.setDoOutput(false);
-			httpUrlConn.setDoInput(true);
-			httpUrlConn.setUseCaches(false);
+            String sendUrl = getUrlWithQueryString(host, params);
 
-			httpUrlConn.setRequestMethod("GET");
-			httpUrlConn.connect();
+            // System.out.println("URL:" + sendUrl);
 
-			// 将返回的输入流转换成字符串
-			InputStream inputStream = httpUrlConn.getInputStream();
-			InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "utf-8");
-			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            URL uri = new URL(sendUrl); // 创建URL对象
+            HttpURLConnection conn = (HttpURLConnection) uri.openConnection();
+            if (conn instanceof HttpsURLConnection) {
+                ((HttpsURLConnection) conn).setSSLSocketFactory(sslcontext.getSocketFactory());
+            }
 
-			String str = null;
-			while ((str = bufferedReader.readLine()) != null) {
-				buffer.append(str);
-			}
-			bufferedReader.close();
-			inputStreamReader.close();
-			// 释放资源
-			inputStream.close();
-			inputStream = null;
-			httpUrlConn.disconnect();
+            conn.setConnectTimeout(SOCKET_TIMEOUT); // 设置相应超时
+            conn.setRequestMethod(GET);
+            int statusCode = conn.getResponseCode();
+            if (statusCode != HttpURLConnection.HTTP_OK) {
+                System.out.println("Http错误码：" + statusCode);
+            }
 
-		} catch (Exception e) {
-		}
-		return buffer.toString();
-	}
+            // 读取服务器的数据
+            InputStream is = conn.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            StringBuilder builder = new StringBuilder();
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                builder.append(line);
+            }
 
-	/**
-	 * utf编码
-	 * 
-	 * @param source
-	 * @return
-	 */
-	public static String urlEncodeUTF8(String source) {
-		String result = source;
-		try {
-			result = java.net.URLEncoder.encode(source, "utf-8");
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
+            String text = builder.toString();
+
+            close(br); // 关闭数据流
+            close(is); // 关闭数据流
+            conn.disconnect(); // 断开连接
+
+            return text;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String getUrlWithQueryString(String url, Map<String, String> params) {
+        if (params == null) {
+            return url;
+        }
+
+        StringBuilder builder = new StringBuilder(url);
+        if (url.contains("?")) {
+            builder.append("&");
+        } else {
+            builder.append("?");
+        }
+
+        int i = 0;
+        for (String key : params.keySet()) {
+            String value = params.get(key);
+            if (value == null) { // 过滤空的key
+                continue;
+            }
+
+            if (i != 0) {
+                builder.append('&');
+            }
+
+            builder.append(key);
+            builder.append('=');
+            builder.append(encode(value));
+
+            i++;
+        }
+
+        return builder.toString();
+    }
+
+    protected static void close(Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 对输入的字符串进行URL编码, 即转换为%20这种形式
+     * 
+     * @param input 原文
+     * @return URL编码. 如果编码失败, 则返回原文
+     */
+    public static String encode(String input) {
+        if (input == null) {
+            return "";
+        }
+
+        try {
+            return URLEncoder.encode(input, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return input;
+    }
+
+    private static TrustManager myX509TrustManager = new X509TrustManager() {
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+        }
+    };
 
 	/**
 	 * 翻译（中->英 英->中 日->中 ）
@@ -78,17 +179,12 @@ public class BaiduTranslateService {
 	 * @return
 	 */
 	public static String translate(String source) {
+		TransApi api = new TransApi(APP_ID, SECURITY_KEY);
 		String dst = null;
-
-		// 组装查询地址
-		String requestUrl = "http://openapi.baidu.com/public/2.0/bmt/translate?client_id=j8pX3RW2MWgP9qLKKrGt25Cc&q={keyWord}&from=auto&to=auto";
-		// 对参数q的值进行urlEncode utf-8编码
-		requestUrl = requestUrl.replace("{keyWord}", urlEncodeUTF8(source));
-
 		// 查询并解析结果
 		try {
 			// 查询并获取返回结果
-			String json = httpRequest(requestUrl);
+			String json = api.getTransResult(source,"auto","auto");
 			// 通过Gson工具将json转换成TranslateResult对象
 			TranslateResult translateResult = new Gson().fromJson(json, TranslateResult.class);
 			// 取出translateResult中的译文
@@ -104,12 +200,6 @@ public class BaiduTranslateService {
 	}
 
 	public static void main(String[] args) {
-		// System.setProperty("http.proxySet", "true");
-		// System.setProperty("http.proxyHost", "proxy.jpn.hp.com");
-		// System.setProperty("http.proxyPort", "8080");
-
-		// 翻译结果：The network really powerful
-		// System.out.println(translate("网络真强大"));
-		System.out.println(translate("明天"));
+		System.out.println(translate("translate"));
 	}
 }
